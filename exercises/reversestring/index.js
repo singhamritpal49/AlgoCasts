8b72c99e40a987fc632a076157efb61b04a58343
Your Name <you@example.com>
2019-11-05 14:45:55 -0500

    String Reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  //Take Str into array
+  // let stringArray = str.split('');
+  // let stringArray = []
+  // stringArray.push(str)
+  // // call reverse method on array
+  // stringArray.reverse();
+  // // Join the array back into a string
+  // // return the result
+  // return stringArray.join('')
 
+  function rev(str) {
+    // .split() takes the string and puts into an array like ["H","E","L","L","O"]
+    let array = str.split("")
+    // .reverse()  makes the array ["O","L","L","E","H"]
+      array.reverse()
+      
+      // .join() makes all the characters join like OLLEH
+      let result = array.join("")
+      return result
+  }
+}
+rev("abc")
 module.exports = reverse;
